c189901917ee80001c12ec44a65a3b5049c60985
dfh420984 <352928736@qq.com>
2019-04-14 22:15:30 +0800

    链表插入pianxu

--- day19/linkList/linkList/main.go
+++ day19/linkList/linkList/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"errors"
 )
 
 //单向链表
@@ -42,6 +43,31 @@ func (this *HeroNode) ListNode(head *HeroNode) {
 	}
 }
 
+//按编号从小到大插入链表
+func  (this *HeroNode) InsertNodeByNo(head *HeroNode, newNode *HeroNode) (err error) {
+	tmpNode := head
+	flag := true
+	for {
+		if tmpNode.next == nil { //到链表最后跳出
+			break
+		} else if tmpNode.next.no > newNode.no {
+			break
+		} else if tmpNode.next.no == newNode.no { //链表中存在就不差入
+			flag = false
+			break
+		}
+		tmpNode = tmpNode.next
+	}
+	if !flag { 
+		errInfo := fmt.Sprintf("链表中以存在该节点:%v", newNode)
+		return errors.New(errInfo)
+	} else { 
+		newNode.next = tmpNode.next
+		tmpNode.next = newNode
+	}
+	return
+}
+
 func main()  {
 	head := &HeroNode{}
 	hero1 := &HeroNode{
@@ -64,9 +90,13 @@ func main()  {
 		name : "吴用",
 		nickname : "智多星",
 	}
-	head.InsertNode(head, hero1)
-	head.InsertNode(head, hero2)
-	head.InsertNode(head, hero3)
-	head.InsertNode(head, hero4)
+	// head.InsertNode(head, hero1)
+	// head.InsertNode(head, hero3)
+	// head.InsertNode(head, hero4)
+	// head.InsertNode(head, hero2)
+	head.InsertNodeByNo(head, hero1)
+	head.InsertNodeByNo(head, hero3)
+	head.InsertNodeByNo(head, hero4)
+	head.InsertNodeByNo(head, hero2)
 	head.ListNode(head)
 }
