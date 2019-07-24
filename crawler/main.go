10dd8541de9d9aaa7cfe18a3b38b5b23cf9d1992
dfh420984 <352928736@qq.com>
2019-07-24 23:45:33 +0800

    爬虫

---
+++ crawler/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/transform"
+)
+
+func main() {
+	resp, err := http.Get("http://www.zhenai.com/zhenghun/")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("StatusOK err %d:\n", http.StatusOK)
+		return
+	}
+	e := determinEncode(resp.Body)
+	utf8Reader := transform.NewReader(resp.Body, e.NewDecoder())
+	all, err := ioutil.ReadAll(utf8Reader)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", all)
+}
+
+func determinEncode(r io.Reader) encoding.Encoding {
+	bytes, err := bufio.NewReader(r).Peek(1024)
+	if err != nil {
+		panic(err)
+	}
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
+}
